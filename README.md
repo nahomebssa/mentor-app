7fdc6fd5f125992750be44a4c98416254f37ad7e
nahomebssa <45636260+nahomebssa@users.noreply.github.com>
2020-08-11 18:09:26 -0400

    Reset branch

--- README.md
+++
@@ -1,4 +0,0 @@
-# Mentor Finder App
-*Your mentor search ends here.*
-
-[Mentor Finder](https://nahomebssa.github.io/mentor-app/)
--- dist/backend/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-  public static void main(String[] args){
-    Database db = new Database();
-    Login log = new Login();
-    log.newUser(db);
-    log.newUser(db);
-    log.existUser(db);
-  }
-}
