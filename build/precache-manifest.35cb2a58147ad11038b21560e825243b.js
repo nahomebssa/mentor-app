26eac94ea2113c9e45c7d3df3cc4c65240a8aed8
nahomebssa <45636260+nahomebssa@users.noreply.github.com>
2020-09-16 21:07:41 -0400

    start gh-pages branch

---
+++ build/precache-manifest.35cb2a58147ad11038b21560e825243b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d00ab9693dc5d9c50b19a7a67a8ed1",
+    "url": "/mentor-app/index.html"
+  },
+  {
+    "revision": "88e0eb5b5b503d008dc4",
+    "url": "/mentor-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "fc01508c6fccc41b7829",
+    "url": "/mentor-app/static/css/main.a27f4e1a.chunk.css"
+  },
+  {
+    "revision": "88e0eb5b5b503d008dc4",
+    "url": "/mentor-app/static/js/2.a9817769.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/mentor-app/static/js/2.a9817769.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc01508c6fccc41b7829",
+    "url": "/mentor-app/static/js/main.e962b9fa.chunk.js"
+  },
+  {
+    "revision": "31b373cc560d2a2f0d53",
+    "url": "/mentor-app/static/js/runtime-main.affe52c1.js"
+  }
+]);
